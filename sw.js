f1bb63483de27bd169c7fad789066d61a232d521
tomshlomo <30502983+tomshlomo@users.noreply.github.com>
2023-10-28 11:06:01 +0000

    Deploying to gh-pages from @ tomshlomo/json-unix-time@ec580dea06ee5e273b0e085dada1008b21fd9119 🚀

---
+++ sw.js
@@ -0,0 +1,25 @@
+var cacheName = 'egui-template-pwa';
+var filesToCache = [
+  './',
+  './index.html',
+  './json_unix_time.js',
+  './json_unix_time_bg.wasm',
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (response) {
+      return response || fetch(e.request);
+    })
+  );
+});
